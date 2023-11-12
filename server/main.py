d5c7f59fc4d2ceb3b6c3c5846cc105b8738683e0
Artem Kondrashov <akondrashov96@gmail.com>
2023-11-12 23:09:48 +0300

    Update main.py and templates
    Update main.py and templates

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from contextlib import asynccontextmanager
 import os
 import io
 import base64
@@ -10,9 +11,7 @@ import matplotlib.pyplot as plt
 import torch
 from transformers import T5ForConditionalGeneration, AutoTokenizer, AutoConfig, PreTrainedTokenizerFast
 
-
-app = FastAPI()
-templates = Jinja2Templates(directory = "templates") 
+corrector = {}
 
 #### Functions block
 
@@ -38,12 +37,29 @@ def load_model():
 def generate_correction(input_text, tokenizer, model):
 
     sentence = "grammar: " + input_text
+    print(input_text)
 
     encodings = tokenizer(sentence, return_tensors="pt").input_ids
-    generated_tokens = model.generate(encodings)
+    generated_tokens = model.generate(encodings, max_length = 150)
     answer = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
 
-    return(answer)
+    indices = [str(int(x != y)) for x, y in zip(input_text.split(), answer.split())]
+
+    return(answer, indices)
+
+asynccontextmanager
+async def lifespan(app: FastAPI):
+    
+    # Load the ML model
+
+    print("Loading model and tokenizer...")
+    corrector['tokenizer'], corrector['model'] = load_model()
+    yield
+
+    corrector.clear()
+
+app = FastAPI(lifespan=lifespan)
+templates = Jinja2Templates(directory = "templates") 
 
 
 #### App Endpoints
@@ -58,12 +74,9 @@ async def root(request: Request):
 @app.get("/gen_correction")
 async def gen_correction(# request: Request, 
                 input_text: str):
-    
-    print("Loading model and tokenizer...")
-    tokenizer, model = load_model()
 
     print("Correcting...")
-    correction = generate_correction(input_text, tokenizer, model)
+    correction, indices = generate_correction(input_text, corrector['tokenizer'], corrector['model'])
 
     return {"correction": correction}
 
@@ -71,14 +84,15 @@ async def gen_correction(# request: Request,
 async def correction(request: Request,
                     input_text: str = Form(...)):
     
-    print("Loading model and tokenizer...")
-    tokenizer, model = load_model()
-
     print("Correcting...")
-    correction = generate_correction(input_text, tokenizer, model)
+    correction, indices = generate_correction(input_text, 
+                                              corrector['tokenizer'], 
+                                              corrector['model'])
+    print(list(zip(correction.split(), indices)))
+    input_text = list(zip(input_text.split(), list(indices)))
+    correction = list(zip(correction.split(), list(indices)))
     
     return templates.TemplateResponse("result.html", 
                                       {"request": request,
                                        "original": input_text,
-                                       "input_text": input_text,
                                        "correction": correction})
